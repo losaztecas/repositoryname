704953011df4f692a113e6aa2a75678823c68370
MarselGaliullin <if741@yandex.ru>
2019-12-14 16:51:55 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    New Laravel project
+                    Project
                 </div>
                 <div>
                     Jenkins edition v.2
