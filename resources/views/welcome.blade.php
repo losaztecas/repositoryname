1302e5c4697ca794c852e92395e794c1a72092e7
losaztecas <57035797+losaztecas@users.noreply.github.com>
2019-12-15 21:34:10 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel - zamena
+                    Gde je sobarica?!1
                 </div>
 
                 <div class="links">
